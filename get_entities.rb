d4eb3ead302c0cfd5919a17c43c190d766a1a521
Ben Chatelain <ben@octop.ad>
2016-07-21 22:48:13 -0600

    Add inital get_entities.rb script

---
+++ get_entities.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+#
+# get_entities.rb
+#
+# Retrieves entities from the Ingress Intel API - https://www.ingress.com/r/getEntities
+#
+
+require 'net/https'
+require 'uri'
+require 'rubygems'
+require 'json'
+require 'FileUtils'
+require 'yaml'
+
+
+def fetch_tile(tile_key)
+  uri = URI.parse("https://www.ingress.com/r/getEntities")
+  referer = "https://www.ingress.com/intel"
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Post.new(uri.request_uri)
+  # request.set_form_data({"q" => "My query", "per_page" => "50"})
+
+  post_body = []
+  post_body << "{"
+  post_body << %Q<"tileKeys":["#{tile_key}"],>
+  post_body << %Q<"v":"3372ba001844bd4a42680f3e6a2372d2490580f9">
+  post_body << "}"
+  # puts "post_body: #{post_body.join}"
+
+  request.body = post_body.join
+  request["Referer"] = referer
+
+  private_data = YAML.load_file('private.yml')
+
+  private_data["Headers"].each do |key, value|
+    request[key] = value
+  end
+
+  cookies = ""
+  private_data["Cookies"].each do |key, value|
+    cookies += "#{key}=#{value}"
+  end
+  request["Cookie"] = cookies
+  # puts cookies
+  # puts request
+
+  response = http.request(request)
+  puts response
+  puts response.body
+
+  # json = JSON.parse(response.body)[0]
+end
+
+fetch_tile("16_6675_12158_0_8_100")
