8eec0d0534dfdb526cd24f7aab6e62bf7ff7c885
Ben Chatelain <ben@octop.ad>
2016-07-26 16:59:26 -0700

    Add handling for 1+ tile keys as arguments to script

--- get_entities.rb
+++ get_entities.rb
@@ -5,6 +5,9 @@
 #
 # Retrieves entities from the Ingress Intel API - https://www.ingress.com/r/getEntities
 #
+# Provide one or more tile keys as arguments to this script.
+# When executed without arguments, uses a default tile key for testing.
+#
 
 require 'net/https'
 require 'uri'
@@ -20,6 +23,9 @@ $version = "3372ba001844bd4a42680f3e6a2372d2490580f9"
 $referer = "https://www.ingress.com/intel"
 base_url = "https://www.ingress.com/r"
 $uri = URI.parse("#{base_url}/getEntities")
+$output_folder = "tiles"
+
+test_tile = "16_6675_12158_0_8_100"
 
 def fetch_tile(tile_key)
   puts "Downloading data for tile #{tile_key}"
@@ -61,7 +67,25 @@ def fetch_tile(tile_key)
   puts ""
   puts response.body
 
-  # json = JSON.parse(response.body)[0]
+  FileUtils::mkdir_p $output_folder
+  File.open("#{$output_folder}/#{tile_key}.json", 'w') {|file| file.write(response.body) }
 end
 
-fetch_tile("16_6675_12158_0_8_100")
+#
+# Parse arguments, build array of tile keys and fetch each one.
+#
+
+tiles_to_download = []
+
+ARGV.each do |arg|
+  puts "Argument: #{arg}"
+  tiles_to_download << arg
+end
+
+if tiles_to_download.to_a.empty?
+  tiles_to_download << test_tile
+end
+
+tiles_to_download.each do |tile|
+  fetch_tile(tile)
+end
